8670c4be379f85322fc1268309b8ffa5b22db88e
Андрей <anastasiiapegar@gmail.com>
2021-07-06 14:48:37 +0300

    Updates

---
+++ precache-manifest.a5021201cac8cecdd3a46f38e31c3bbb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a4b7bf4a56eff2b4fafa112814934a",
+    "url": "/Moskvin_portfolio/index.html"
+  },
+  {
+    "revision": "9072bb33fc7ca8f27442",
+    "url": "/Moskvin_portfolio/static/css/main.4c10253e.chunk.css"
+  },
+  {
+    "revision": "885d9924f078388a4083",
+    "url": "/Moskvin_portfolio/static/js/2.0175647e.chunk.js"
+  },
+  {
+    "revision": "5e7a3bbd1766925a2d65977472adeaa7",
+    "url": "/Moskvin_portfolio/static/js/2.0175647e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9072bb33fc7ca8f27442",
+    "url": "/Moskvin_portfolio/static/js/main.c14e9712.chunk.js"
+  },
+  {
+    "revision": "4b34c5f2582cae1ee8b5",
+    "url": "/Moskvin_portfolio/static/js/runtime-main.0a474280.js"
+  },
+  {
+    "revision": "de076b03ffca3c00388ff32587bd3026",
+    "url": "/Moskvin_portfolio/static/media/avatar.de076b03.jpg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/Moskvin_portfolio/static/media/linkedin-in-brands.1e01f0c0.svg"
+  },
+  {
+    "revision": "32461c64f5a31beabc222b11b0582b58",
+    "url": "/Moskvin_portfolio/static/media/social-media.32461c64.png"
+  },
+  {
+    "revision": "a61ad47354d222c6ad65ef0497c2f4c8",
+    "url": "/Moskvin_portfolio/static/media/social-network-png-free.a61ad473.png"
+  },
+  {
+    "revision": "bdc0fc45ab7bc8f910b9714e7b8f8bc0",
+    "url": "/Moskvin_portfolio/static/media/telegram-plane-brands.bdc0fc45.svg"
+  }
+]);

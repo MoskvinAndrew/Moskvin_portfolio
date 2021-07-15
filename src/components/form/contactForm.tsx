546f37ab6c2dd2659b77939acf39808967a32f24
Андрей <anastasiiapegar@gmail.com>
2021-07-15 08:21:41 +0300

    feedback form comleted

--- src/components/form/contactForm.tsx
+++ src/components/form/contactForm.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import styled,{css} from "styled-components";
+import { useFormik} from 'formik';
+import axios from "axios";
+import * as Yup from 'yup';
+
+
 
 const StyledFormInner = styled.div`
   max-width: 1320px;
   margin: 0 auto;
+  padding: 0px 20px 0px 20px; 
+`
+const DivStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+`
+const ErrorMessage = styled.p`
+  position: absolute;
+  top:15px;
+  font-size: 16px;
+  color:darkred;
+  text-transform: uppercase;
+ 
 `
 const H2styled = styled.h2`
   border-bottom: 1px solid #e0e0e0;
@@ -15,17 +34,146 @@ const H2styled = styled.h2`
   text-align: left;
   font-size: 32px;
 `
+const InputStyled = styled.input`
+  height: 40px;
+  width: 100%;
+  margin-bottom: 30px;
+
+  &::placeholder {
+    color: #c2c0c0;
+  }
+
+  &:focus::placeholder {
+    color: transparent;
+  }
+`
+const TextArea = styled.textarea`
+   width: 100%;
+   height: 100px;
+   resize: none;
+`
+const ButtonStyled = styled.button`
+  color: #878585;
+  padding: 10px 10px;
+  cursor: pointer;
+  background-color: #f6f5f5;
+  border-color: #cecccc;
+  margin-top: 30px;
+  float: left;
+
+  &:hover {
+    border-color: #3e3e3e;
+  }
+
+`
 
 
 export const ContactForm = () =>{
-    return (
-        <>
-        <StyledFormInner >
-    <H2styled>LET'S KEEP IN TOUCH</H2styled>
-        </StyledFormInner>
-        <form>
 
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            subject: '',
+            email: '',
+            message: ''
+        },
+
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .max(15, 'Must be 15 characters or less')
+                .required('please tape your name'),
+
+            email: Yup.string()
+                .email('Invalid email address')
+                .required('email is required'),
+            message:Yup.string()
+                .required('Required')
+        }),
+
+        onSubmit:(values, {resetForm}) => {
+
+            console.log(values)
+            axios.post("https://gmail--nodejs--server.herokuapp.com/sendMessage",{
+                 values
+
+            })
+            resetForm();
+
+        },
+    });
+    return (
+
+        <StyledFormInner>
+            <H2styled>
+                LET'S KEEP IN TOUCH
+            </H2styled>
+
+
+        <form onSubmit={formik.handleSubmit }>
+            <label htmlFor="name"></label>
+            <DivStyled>
+
+            <InputStyled
+                style={{marginRight:"20px"}}
+                placeholder='*Name'
+                id="name"
+                name="name"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.name}
+            />
+            {formik.touched.name && formik.errors.name ? (
+                <ErrorMessage>{formik.errors.name}</ErrorMessage>
+            ) : null}
+
+
+            <label htmlFor="email"></label>
+
+            <InputStyled
+                placeholder='*Email address'
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? (
+                <ErrorMessage style={{right:"0px"}}>{formik.errors.email}</ErrorMessage>
+            ) : null}
+
+            </DivStyled>
+            <label htmlFor="subject"></label>
+            <InputStyled
+                placeholder={"Subject"}
+                id="subject"
+                name="subject"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.subject}
+            />
+            {formik.touched.subject && formik.errors.subject ? (
+                <p>{formik.errors.subject}</p>
+            ) : null}
+            <DivStyled>
+            <label htmlFor="message"></label>
+            <TextArea
+                placeholder="*Message"
+                id="message"
+                name="message"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.message}
+            />
+            {formik.touched.message && formik.errors.message ? (
+                <ErrorMessage style={{top:'80px'}}>{formik.errors.message}</ErrorMessage>
+            ) : null}
+            </DivStyled>
+            <ButtonStyled type="submit">Send Message</ButtonStyled>
         </form>
-          </>
-    )
+        </StyledFormInner>
+    );
 }
+
